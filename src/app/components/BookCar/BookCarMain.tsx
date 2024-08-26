9eb7e3973bc6a8966688154911c4f81a834c310d
iknoul <rhmanshamil@gmail.com>
2024-08-26 21:43:10 +0530

    title and readme edited

--- src/app/components/BookCar/BookCarMain.tsx
+++ src/app/components/BookCar/BookCarMain.tsx
@@ -1,9 +1,15 @@
 'use client'
 import { useState } from 'react'
-import style from './styles/BookCarMain.module.css'
+
 import AppPromotion from './AppPromotion'
 import InputFields from './InputFields'
 import TypeSelector from './TypeSelector'
+
+import Image from 'next/image'
+import chatIcon from '@/../public/icons/chatIcon.svg'
+import callIcon from '@/../public/icons/callIcon.svg'
+import style from './styles/BookCarMain.module.css'
+
 
 const BookCarMain:React.FC = ()=>{
 
@@ -12,6 +18,21 @@ const BookCarMain:React.FC = ()=>{
 
     return(
     <div className={style.bookCarMain}>
+
+        <button className={`${style.chat} ${style.call}`}>
+            <Image 
+                src={callIcon}
+                alt='call icon'
+            />
+        </button>
+
+        <button className={`${style.chat} ${style.bot}`}>
+            <Image 
+                    src={chatIcon}
+                    alt='chat icon'
+            />
+        </button>
+
         <TypeSelector  differentDropOff = {differentDropOff} setDifferentDropOff = {setDifferentDropOff}/>
         <InputFields differentDropOff={differentDropOff}/>
         <AppPromotion />
